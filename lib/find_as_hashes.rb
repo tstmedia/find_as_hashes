e8dd32562bd57c16bd44ac0efff9966ad5af90fc
Patrick Byrne <patrick.byrne@tstmedia.com>
2011-09-19 15:18:01 -0500

    Reorganize how hash-finding methods are defined. Fixes #1

--- lib/find_as_hashes.rb
+++ lib/find_as_hashes.rb
@@ -1,13 +1,14 @@
 require "find_as_hashes/version"
 
-module ActiveRecord
-  module FinderMethods
-    def all_as_hashes
-      connection.select_all(to_sql)
-    end
+module FindAsHashes
+  def all_as_hashes
+    # TODO figure out a less hacky way of retrieveing existing relation stack and not modifying it than where(nil)
+    connection.select_all(where(nil).to_sql)
+  end
 
-    def first_as_hash
-      connection.select_one(limit(1).to_sql)
-    end
+  def first_as_hash
+    connection.select_one(limit(1).to_sql)
   end
 end
+
+ActiveRecord::Base.extend FindAsHashes
